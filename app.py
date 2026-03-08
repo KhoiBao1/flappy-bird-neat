3b6ee68eee86db1ec01fd6afeb3ef88593814e10
Khoi Bao <khoibao928@gmail.com>
2026-03-07 22:21:14 -0800

    fix port for render

--- app.py
+++ app.py
@@ -287,5 +287,5 @@ def action():
     return jsonify({'status':'ok'})
 
 if __name__=='__main__':
-    port=int(os.environ.get('PORT',5000))
+    port=int(os.environ.get('PORT',10000))
     app.run(host='0.0.0.0',port=port,threaded=True)
